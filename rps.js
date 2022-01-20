6e3943e4aa6266cdad24f7031ec8243e7431c2c6
Brett Bylsma <brettbylsma1990@gmail.com>
2022-01-19 19:35:12 -0500

    made changes to js file to make output more clear

--- rps.js
+++ rps.js
@@ -13,22 +13,22 @@ let computerScore = 0
 function playRound(playerSelection, computerSelection) {
   if (playerSelection=='rock' && computerSelection=='scissors') {
     playerScore++
-    return  `You win, rock beats scissors! Score: ${playerScore} to ${computerScore}`
+    return  `You win this round, You picked rock, computer picked scissors! Score: ${playerScore} to ${computerScore}`
     } else if (playerSelection=='scissors' && computerSelection=='paper') {
       playerScore++
-      return `You win, scissors beats paper! Score: ${playerScore} to ${computerScore}`
+      return `You win this round, you picked scissors, computer picked paper! Score: ${playerScore} to ${computerScore}`
     } else if (playerSelection=='paper' && computerSelection=='rock') {
       playerScore++
-      return `You win, paper beats rock! Score: ${playerScore} to ${computerScore}`
+      return `You win this round, you picked paper, computer picked rock! Score: ${playerScore} to ${computerScore}`
     } else if (playerSelection=='rock' && computerSelection=='paper') {
       computerScore++
-      return `You lose, paper beats rock! Score: ${playerScore} to ${computerScore} `
+      return `You lose this round, you picked rock, computer picked paper! Score: ${playerScore} to ${computerScore} `
     } else if (playerSelection=='scissors' && computerSelection=='rock') {
       computerScore++
-      return `You lose, rock beats scissors! Score: ${playerScore} to ${computerScore} `
+      return `You lose this round, you picked scissors, computer picked rock! Score: ${playerScore} to ${computerScore} `
     } else if (playerSelection=='paper' && computerSelection=='scissors') {
       computerScore++
-      return `You lose, scissors beats paper! Score: ${playerScore} to ${computerScore} `
+      return `You lose this round, you picked paper, computer picked scissors! Score: ${playerScore} to ${computerScore} `
     } else return `It's a tie!  Try again.  Score: ${playerScore} to ${computerScore}`
   }
   
@@ -36,7 +36,6 @@ function game() {
   for (let step=0; step <5; step++) {
     const computerSelection=computerPlay();
     let playerSelection=prompt('Rock, Paper, or Scissors');
-    playerSelection=playerSelection.toLowerCase();
     console.log(playRound(playerSelection, computerSelection));
   }
  if (playerScore>computerScore) {
